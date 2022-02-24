7ec4b72e140552f03221d6615a0bc26206602315
Dennis Maina <maina@dentricedev.com>
2022-02-25 02:12:34 +0300

    comment out ngrok to run locally by default

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ words = pickle.load(open("words.pkl", "rb"))
 classes = pickle.load(open("classes.pkl", "rb"))
 
 app = Flask(__name__)
-run_with_ngrok(app) 
+# run_with_ngrok(app) 
 
 @app.route("/")
 def home():
